90576bf861e4b33685b03372b646cf5c64195787
tomi <remioluwatomicephas@gmail.com>
2025-02-19 12:33:41 +0100

    feat: Implemented the TrackAsync method for PerformanceTracker service

--- TelexAPM/Services/PerformanceTracker.cs
+++ TelexAPM/Services/PerformanceTracker.cs
@@ -18,6 +18,33 @@ internal class PerformanceTracker : BaseTracker, IPerformanceTracker
 
     public async Task TrackAsync(string metricName, TimeSpan duration)
     {
+        if (string.IsNullOrEmpty(metricName)) throw new ArgumentNullException(nameof(metricName), "Metric name cannot be empty");
+
+        try
+        {
+            var performanceReport = new PerformanceReport
+            {
+                Message = "",
+                Duration = duration.ToString(),
+            };
+
+            var payload = new WebhookPayload
+            {
+                EventName = $"{metricName} POST",
+                Message = JsonConvert.SerializeObject(performanceReport),
+                Status = "success",
+                Username = "channel"
+            };
+
+            await Track(payload);
+
+        }
+        catch (Exception exception)
+        {
+            // telex is always meant to succeed as per hng - task req; hence no catch implementation.
+            Console.WriteLine(exception);
+        }
+
 
     }
 }
